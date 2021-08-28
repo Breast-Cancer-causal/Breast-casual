2a50ea4da939ee2cb694dc75ef2433489491d0c1
smegnshd <smegnshdem@gmail.com>
2021-08-28 08:46:27 +0300

    setup file

---
+++ setup.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 28 08:23:31 2021
+
+@author: Smegn
+"""
+
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+with open('requirements.txt') as requirement_file:
+    requirements_list = requirement_file.readlines()
+    requirements_list = [lib.replace('\n', '') for lib in requirements_list]
+
+requirements = requirements_list
+
+test_requirements = []
+
+setup(
+    author="Smegnsh, Boris, Saba,David",
+    author_email="smegnshdem@gmail.com",
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    description="Constructing a Casual Graph and based on graph it will predict model performance for Breast Cancer Disease.",
+    install_requires=requirements,
+    long_description=readme,
+    include_package_data=True,
+    keywords='scripts',
+    name='scripts',
+    packages=find_packages(include=['scripts', 'scripts.*']),
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/Breast-Cancer-causal/Dashboard-casual',
+    version='0.1.0',
+    zip_safe=False,
+)
